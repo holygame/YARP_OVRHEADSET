0163614d4ac8e612af0efd553d38a1ccdba4dde9
lpenco <luigi.penco@inria.fr>
2018-08-28 13:21:54 +0200

    All in one module

--- OculusInAction.cpp
+++ OculusInAction.cpp
@@ -4,8 +4,8 @@
 #include <thread>
 #include <iostream>
 #include <yarp/dev/IJoypadController.h>
-#include "Cameras.h"
-#include "JoySticksStream.h"
+#include "OculusInAction.h"
+
 
 #define DEBUG 0
 //If debug is set to true , you"ll see the cameras as OpenCV images 
@@ -13,35 +13,41 @@
 using namespace std::literals::chrono_literals;
 
 
-int main(int argc , char** argv)
-{
+ void MyModule::AddAxis(Bottle& b, int n, double out)
+ {
+	 joyPad->getAxis(n, out);
+	 b.addDouble(out);
+ }
+ void MyModule::AddButton(Bottle& b, int n, float out)
+ {
+	 joyPad->getButton(n, out);
+	 b.addDouble(out);
+ }
 
-	Network yarp;
+ void MyModule::AddStick(Bottle& b, int n, Vector& out, IJoypadController::JoypadCtrl_coordinateMode type)
+ {
+	 joyPad->getStick(n, out, type);
+	 for (int i = 0; i < out.length(); i++)
+	 {
+		 b.addDouble(out[i]);
+	 }
+ }
+
+bool MyModule::configure(yarp::os::ResourceFinder &rf)
+{
 	leftport.open("/oculus/joysticks/left:o");
 	rightport.open("/oculus/joysticks/right:o");
 	leftStickPort.open("/oculus/joysticks/left/stick:o");
 	rightStickPort.open("/oculus/joysticks/right/stick:o");
-
-	if (!yarp.checkNetwork())
-
-	{
-		printf("YARP server not available!\n");
-		return 1;
-	}
-
-	ResourceFinder rf;
-	rf.setVerbose(true);
-	rf.setDefault("name", "Grabber");
-	rf.setDefault("robot", "icubSim");
-	//rf.setDefault("robot", "iCubNancy01");
-	rf.setDefaultConfigFile("myModule.ini");
-	rf.setDefaultContext("myModule/conf");
-	rf.configure(argc, argv);
+	/*leftport.open("/oculus/local/display/right:i");
+	rightport.open("/oculus/local/display/left:i");
+	Network::connect("/oculus/local/display/right:i", "/oculus/display/right:i");
+	Network::connect("/oculus/local/display/left:i", "/oculus/display/left:i");*/
+	
 	string moduleName = rf.find("name").asString();
 	string robotName = rf.find("robot").asString();
 	inputPortL = "/" + robotName + "/cam/left"; // icub or icubsim
 	inputPortR = "/" + robotName + "/cam/right";
-
 	if (DEBUG)
 	{
 		cvNamedWindow("rightview", 1);
@@ -53,7 +59,7 @@ int main(int argc , char** argv)
 		options.fromConfigFile("ovr.ini");
 		if (!headset.open(options))
 		{
-			printf("ovrheadset not ready\n");
+			LOG("ovrheadset not ready");
 			return false;
 		}
 		if (!headset.view(joyPad))
@@ -61,6 +67,7 @@ int main(int argc , char** argv)
 			LOG("cant acces joysticks");
 			return false;
 		}
+		
 
 	}
 	//------------------Opening Cameras driver----------------------//
@@ -94,101 +101,131 @@ int main(int argc , char** argv)
 		LOG("unable to dynamic cast remote_grabber to IFrameGrabber interface ");
 		return false;
 	}
-	/*leftport.open("/oculus/local/display/right:i");
-	rightport.open("/oculus/local/display/left:i");
-	Network::connect("/oculus/local/display/right:i", "/oculus/display/right:i");
-	Network::connect("/oculus/local/display/left:i", "/oculus/display/left:i");*/
-	while (true)
+	
+}
+bool MyModule::interruptModule()
+{
+	return true;
+}
+bool MyModule::close()
+{
+
+	if (!DEBUG)
+		headset.close();
+	camL.close();
+	camR.close();
+	leftport.close();
+	rightport.close();
+	leftStickPort.close();
+	rightStickPort.close();
+	delete joyPad;
+	delete IframR;
+	delete IframL;
+	return true;
+}
+
+bool MyModule::respond(const Bottle& command, Bottle& reply)
+{
+	return true;
+}
+
+double MyModule::getPeriod()
+{
+	return 0.05;
+}
+
+bool MyModule::updateModule()
+{
+	//std::this_thread::sleep_for(100ms);
+	Bottle& bLeft = leftport.prepare();
+	Bottle& bRight = rightport.prepare();
+	Bottle& bRightStick = rightStickPort.prepare();
+	Bottle& bLeftStick = leftStickPort.prepare();
+	bRight.clear();
+	bLeft.clear();
+	bLeftStick.clear();
+	bRightStick.clear();
+
+	// ADD BUTTON //
+	for (int i = 0; i < 3; i++)
 	{
-		//std::this_thread::sleep_for(100ms);
-		Bottle& bLeft = leftport.prepare();
-		Bottle& bRight = rightport.prepare();
-		Bottle& bRightStick = rightStickPort.prepare();
-		Bottle& bLeftStick = leftStickPort.prepare();
-		bRight.clear();
-		bLeft.clear();
-		bLeftStick.clear();
-		bRightStick.clear();
-
-		// ADD BUTTON //
-		for (int i = 0; i < 3; i++)
+		AddButton(bRight, i, outFloat);
+	}
+	for (int i = 4; i < 7; i++)
+	{
+		AddButton(bLeft, i, outFloat);
+	}
+	// ADD AXIS //
+	AddAxis(bRight, 1, outDouble);
+	AddAxis(bRight, 3, outDouble);
+	AddAxis(bLeft, 0, outDouble);
+	AddAxis(bLeft, 2, outDouble);
+	// ADD STICKS //
+	AddStick(bLeftStick, 0, leftVector);
+	AddStick(bRightStick, 1, rightVector);
+	leftStickPort.write();
+	rightStickPort.write();
+	leftport.write();
+	rightport.write();
+	if (DEBUG)
+	{
+		ImageOf<PixelRgb> imgL;
+		if (IframL == nullptr)
 		{
-			AddButton(bRight, i, outFloat);
+			printf("NULLPTR_ERROR");
+			return false;
 		}
-		for (int i = 4; i < 7; i++)
+		if (IframL->getImage(imgL))
 		{
-			AddButton(bLeft, i, outFloat);
+
+			std::cout << " imgsize is: width  " << imgL.width() << " height : " << imgL.height() << std::endl;
+			IplImage *cvImage = cvCreateImage(cvSize(imgL.width(),
+				imgL.height()),
+				IPL_DEPTH_8U, 3);
+			cvCvtColor((IplImage*)imgL.getIplImage(), cvImage, CV_RGB2BGR);
+
+			cvShowImage("leftview", cvImage);
+			cvWaitKey(5);
 		}
-		// ADD AXIS //
-		AddAxis(bRight, 1, outDouble);
-		AddAxis(bRight, 3, outDouble);
-		AddAxis(bLeft, 0, outDouble);
-		AddAxis(bLeft, 2, outDouble);
-		// ADD STICKS //
-		AddStick(bLeftStick, 0, leftVector);
-		AddStick(bRightStick, 1, rightVector);
-		leftStickPort.write();
-		rightStickPort.write();
-		leftport.write();
-		rightport.write();
-		if (DEBUG)
+		ImageOf<PixelRgb> imgR;
+		if (IframR == nullptr)
 		{
-			ImageOf<PixelRgb> imgL;
-			if (IframL == nullptr)
-			{
-				printf("NULLPTR_ERROR");
-				return false;
-			}
-			if (IframL->getImage(imgL))
-			{
+			printf("NULLPTR_ERROR");
+			return false;
+		}
+		if (IframR->getImage(imgR))
+		{
 
-				std::cout << " imgsize is: width  " << imgL.width() << " height : " << imgL.height() << std::endl;
-				IplImage *cvImage = cvCreateImage(cvSize(imgL.width(),
-					imgL.height()),
-					IPL_DEPTH_8U, 3);
-				cvCvtColor((IplImage*)imgL.getIplImage(), cvImage, CV_RGB2BGR);
+			std::cout << " imgsize is: width  " << imgR.width() << " height : " << imgR.height() << std::endl;
+			IplImage *cvImage = cvCreateImage(cvSize(imgR.width(),
+				imgR.height()),
+				IPL_DEPTH_8U, 3);
+			cvCvtColor((IplImage*)imgR.getIplImage(), cvImage, CV_RGB2BGR);
 
-				cvShowImage("leftview", cvImage);
-				cvWaitKey(5);
-			}
-			ImageOf<PixelRgb> imgR;
-			if (IframR == nullptr)
-			{
-				printf("NULLPTR_ERROR");
-				return false;
-			}
-			if (IframR->getImage(imgR))
-			{
-
-				std::cout << " imgsize is: width  " << imgR.width() << " height : " << imgR.height() << std::endl;
-				IplImage *cvImage = cvCreateImage(cvSize(imgR.width(),
-					imgR.height()),
-					IPL_DEPTH_8U, 3);
-				cvCvtColor((IplImage*)imgR.getIplImage(), cvImage, CV_RGB2BGR);
-
-				cvShowImage("rightview", cvImage);
-				cvWaitKey(5);
-			}
+			cvShowImage("rightview", cvImage);
+			cvWaitKey(5);
 		}
 	}
-	return 0;
- }
- void AddAxis(Bottle& b, int n, double out)
- {
-	 joyPad->getAxis(n, out);
-	 b.addDouble(out);
- }
- void AddButton(Bottle& b, int n, float out)
- {
-	 joyPad->getButton(n, out);
-	 b.addDouble(out);
- }
+	return true;
+}
 
- void AddStick(Bottle& b, int n, Vector& out, IJoypadController::JoypadCtrl_coordinateMode type)
- {
-	 joyPad->getStick(n, out, type);
-	 for (int i = 0; i < out.length(); i++)
-	 {
-		 b.addDouble(out[i]);
-	 }
- }
+int main(int argc, char** argv)
+{
+	Network yarp;
+	if (!yarp.checkNetwork())
+
+	{
+		LOG("YARP server not available!");
+		return false;
+	}
+	ResourceFinder rf;
+	rf.setVerbose(true);
+	rf.setDefault("name", "Grabber");
+	rf.setDefault("robot", "icubSim");
+	//rf.setDefault("robot", "iCubNancy01");
+	rf.setDefaultConfigFile("myModule.ini");
+	rf.setDefaultContext("myModule/conf");
+	rf.configure(argc, argv);
+	MyModule myModule;
+	return myModule.runModule(rf);
+}
---
+++ OculusInAction.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <yarp/os/all.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Thread.h>
+#include <yarp/dev/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/dev/IJoypadController.h>
+
+
+#define LOG(x) std::cout << x << std::endl;
+using std::string;
+using namespace yarp::os;
+using namespace yarp::dev;
+using namespace yarp::sig;
+
+
+
+class MyModule :public RFModule
+{
+	//CAMERAS
+	string robotName;
+	string inputPortL;
+	string inputPortR;
+	string localPortL;
+	string localPortR;
+	PolyDriver camL;
+	PolyDriver camR;
+	PolyDriver headset;
+	ImageOf<PixelRgb> img;
+	IFrameGrabberImage *IframR;
+	IFrameGrabberImage *IframL;
+	//JOYSTICKS
+	float outFloat = .0f;
+	double outDouble = 0;
+	Vector leftVector;
+	Vector rightVector;
+	BufferedPort<Bottle> leftport;
+	BufferedPort<Bottle> rightport;
+	BufferedPort<Bottle> leftStickPort;
+	BufferedPort<Bottle> rightStickPort;
+	IJoypadController *joyPad;
+	
+	
+public:
+
+	bool configure(yarp::os::ResourceFinder &rf);
+	bool interruptModule();                   
+	bool close();                                
+	bool respond(const Bottle& command, Bottle& reply);
+	double getPeriod();
+	bool updateModule();
+	//JOYSTICKS
+	//Function to write the button pressed on a Bottle
+	void AddButton(Bottle& b, int n, float out);
+	//Function to write the trigger pressur  on a Bottle
+	void AddAxis(Bottle& b, int n, double out);
+	//Function to write the position of the axis on a Bottle
+	//CARTESIAN coordinates are used by default but can be changed to POLAR
+	void AddStick(Bottle& b, int n, Vector& out, IJoypadController::JoypadCtrl_coordinateMode type = IJoypadController::JypCtrlcoord_CARTESIAN);
+};
